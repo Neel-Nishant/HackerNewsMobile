ebdd72097f6cb42b9e456ce9b68f17d645d9ea30
Neel-Nishant <neeljvm@gmail.com>
2018-04-13 11:15:34 +0530

    Initial Commit

---
+++ HackerNewsExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNewsExercise
+//
+//  Created by Neel Nishant on 13/04/18.
+//  Copyright © 2018 Neel Nishant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
